63a13c935d034e3a74c969136a47a6308f178f69
Adil Ishenkulov <adil@rss.app>
2025-03-10 04:40:20 +0600

    half finish

--- src/blocks/philosophy/Philosophy.tsx
+++ src/blocks/philosophy/Philosophy.tsx
@@ -1,20 +1,105 @@
 'use client';
 
+import React from 'react';
+import { gsap } from 'gsap';
+import { ScrollTrigger } from 'gsap/ScrollTrigger';
 import global from '@/src/styles/global_styles.module.css';
 import styles from './philosophy.module.css';
 import Image from 'next/image';
+import clsx from 'clsx';
 
+gsap.registerPlugin(ScrollTrigger);
 function Philosophy() {
+	const mainRef = React.useRef(null);
+	const boxRef = React.useRef(null);
+	const boxRef2 = React.useRef(null);
+	const boxRef3 = React.useRef(null);
+	const boxRef4 = React.useRef(null);
+	React.useEffect(() => {
+		ScrollTrigger.defaults({
+			scroller: mainRef.current,
+		});
+		gsap.to(boxRef.current, {
+			opacity: 0,
+			scale: 0.9,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+		gsap.to(boxRef2.current, {
+			y: -760,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+		gsap.to(boxRef3.current, {
+			y: -1520,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef2.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+		gsap.to(boxRef2.current, {
+			opacity: 0,
+			scale: 0.9,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef2.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+		gsap.to(boxRef4.current, {
+			y: -2280,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef3.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+		gsap.to(boxRef3.current, {
+			opacity: 0,
+			scale: 0.9,
+			duration: 2,
+			ease: 'power1.inOut',
+			scrollTrigger: {
+				trigger: boxRef3.current,
+				start: 'bottom 30%', // Анимация начинается, когда элемент на 80% виден
+				end: 'top 30%',
+				scrub: 1, // Привязка анимации к скроллу
+			},
+		});
+	}, []);
+
 	return (
-		<section className={styles.main}>
-			<div className={global.container}>
+		<section className={styles.main} ref={mainRef}>
+			<div className={clsx(global.container, styles.wrapper)}>
 				<h2 className={styles.title}>Философия</h2>
 				<p className={styles.description}>
 					Философия нашего курорта воплощена в четырех ключевых принципах,
 					которые создают уникальную атмосферу для каждого гостя и жителя.
 				</p>
 				<div className={styles.content}>
-					<div className={styles.block}>
+					<div className={styles.block} ref={boxRef}>
 						<Image
 							src='/images/image-1.png'
 							alt='Image'
@@ -35,7 +120,7 @@ function Philosophy() {
 							</p>
 						</div>
 					</div>
-					<div className={styles.block}>
+					<div className={styles.block} ref={boxRef2}>
 						<Image
 							src='/images/image-1.png'
 							alt='Image'
@@ -54,7 +139,7 @@ function Philosophy() {
 							</p>
 						</div>
 					</div>
-					<div className={styles.block}>
+					<div className={styles.block} ref={boxRef3}>
 						<Image
 							src='/images/image-1.png'
 							alt='Image'
@@ -76,7 +161,7 @@ function Philosophy() {
 							</p>
 						</div>
 					</div>
-					<div className={styles.block}>
+					<div className={styles.block} ref={boxRef4}>
 						<Image
 							src='/images/image-1.png'
 							alt='Image'
